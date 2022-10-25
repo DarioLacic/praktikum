af5f402232c5d2ae764f5b2c810f6bb806fa05e3
DarioLacic <116653050+DarioLacic@users.noreply.github.com>
2022-10-25 15:36:40 +0200

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Praktikum bei Krankikom";
+  }
+  function myFunction1() {
+    document.getElementById("demo").innerHTML = "Praktikum <br>25.10";
+  }
+   
